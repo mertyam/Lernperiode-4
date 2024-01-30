125d14300a79bd76a178b5d01b6cf2853081e7fb
mertyam <142886154+mertyam@users.noreply.github.com>
2024-01-30 10:19:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        // Verbindungsinformationen zum SQL Server
+        string server = "DESKTOP-HR8IHM9\\SQLEXPRESS";
+        string database = "MertYamaliDB";
+        string username = "";
+        string password = "465020";
+
+        // Verbindungsschnur (Connection String) erstellen
+        string connectionString = $"Server={server};Database={database};User Id={username};Password={password};";
+
+        // SqlConnection-Objekt erstellen
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                // Verbindung öffnen
+                connection.Open();
+                Console.WriteLine("Verbindung erfolgreich geöffnet.");
+
+                
+
+                // Beispiel: SQL-Befehl ausführen
+                string sqlQuery = "SELECT * FROM DeineTabelle";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Verarbeitung der Daten hier
+                            Console.WriteLine(reader["Vorname"].ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler beim Öffnen der Verbindung: {ex.Message}");
+            }
+        }
+    }
+}
